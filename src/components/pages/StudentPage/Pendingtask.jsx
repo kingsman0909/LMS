ed55d2425848578283c3edca87af2d904883242d
kingsman0909 <kencyrusorilleneda@gmail.com>
2026-09-02 07:49:36 +0800

    fixed env variable name in frontend

--- src/components/pages/StudentPage/Pendingtask.jsx
+++ src/components/pages/StudentPage/Pendingtask.jsx
@@ -7,7 +7,7 @@ import "../../../styles/StudentPage.css";
 import PdfPreview from "./PdfPreview";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
+  import.meta.env.VITE_API_URL ||
   "http://localhost:3000";
 
 function PendingTask({
